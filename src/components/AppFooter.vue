6180fb537aee74641774461367304235375de83a
SuperSonic <supersonic@livemail.tw>
2023-09-10 06:45:29 +0000

    feat: init vue3

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -8,13 +8,7 @@
       <a class="mr-3" href="https://web-tech-tw.github.io/#/privacy">隱私權政策</a>
     </div>
     <div class="max-w-7xl mx-auto mt-5 divide-y divide-gray-200 px-4 sm:px-6 md:px-8">
-      &copy; 2022 臺灣網際網路技術推廣組織 Taiwan Web Technology Promotion Organization (Web Tech TW)
+      &copy; 2023 臺灣網際網路技術推廣組織 Taiwan Web Technology Promotion Organization (Web Tech TW)
     </div>
   </footer>
 </template>
-
-<script>
-export default {
-  name: 'AppFooter'
-}
-</script>
