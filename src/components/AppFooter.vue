520e5c377b9a3d5dfc1fd931fd9f027fabd37014
SuperSonic <supersonic@livemail.tw>
2023-09-25 03:26:02 +0000

    feat: make year auto-update in footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -8,7 +8,11 @@
       <a class="mr-3" href="https://web-tech-tw.github.io/#/privacy">隱私權政策</a>
     </div>
     <div class="max-w-7xl mx-auto mt-5 divide-y divide-gray-200 px-4 sm:px-6 md:px-8">
-      &copy; 2023 臺灣網際網路技術推廣組織 Taiwan Web Technology Promotion Organization (Web Tech TW)
+      &copy; {{year}} 臺灣網際網路技術推廣組織 Taiwan Web Technology Promotion Organization (Web Tech TW)
     </div>
   </footer>
 </template>
+
+<script setup>
+const year = new Date().getFullYear();
+</script>
