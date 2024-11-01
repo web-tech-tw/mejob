e78fc86d2f96e40ce0a2dcd6c826d0a27188da06
supersonictw <supersonic@livemail.tw>
2024-11-01 17:45:22 +0800

    Deploying to dist-pages from @ web-tech-tw/mejob@e200ff22d61b1e8610f588a28d9712e33c518e28 🚀

---
+++ assets/CareersView-ad06c40a.js
@@ -0,0 +1,1 @@
+import{_ as n,o as c,c as t,b as s}from"./index-5e1aae48.js";const o={},r={class:"flex justify-center py-32"};function a(i,e){return c(),t("div",r,e[0]||(e[0]=[s("div",{class:"inline-block"},[s("span",{class:"inline ml-2"},"一生懸命開發中 ٩(๑•̀ω•́๑)۶")],-1)]))}const _=n(o,[["render",a]]);export{_ as default};
